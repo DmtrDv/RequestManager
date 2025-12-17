f4b1833888c310dade540375301c384a4b44440e
DmtrDv <dmitriybychkov75146@gmail.com>
2025-12-17 15:13:00 +0300

    создал модель заказа

---
+++ RequestManager/RequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestManager
+{
+    public class RequestModel
+    {
+        [DisplayName("ИД")]
+        public int Id_Request { get; } // id 
+        [DisplayName("Заказчик")]
+        public string Сustomer { get; set; }
+        [DisplayName("Дата заявки")]
+        public DateTime RequestDate { get; set; }
+        [DisplayName("Состояние")]
+        public string Condition { get; set; }
+        [DisplayName("Описание требований")]
+        public string Description { get; set; }
+    }
+}
