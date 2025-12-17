1e7271031ae8238aa9beeaa47d396bb5282dba5d
DmtrDv <dmitriybychkov75146@gmail.com>
2025-12-17 15:31:17 +0300

    добавил интерфейс

---
+++ RequestManager/IRequestManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestManager
+{
+    public interface IRequestManager
+    {
+        string AddRequest(RequestModel requestModel);
+        string UpdateRequests(RequestModel requestModel);
+    }
+}
