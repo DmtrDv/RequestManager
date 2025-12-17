4ec1d6d5332cdf422708135c667a4125c151bfbd
DmtrDv <dmitriybychkov75146@gmail.com>
2025-12-17 16:04:50 +0300

    добавил менеджер заявки

---
+++ RequestManager/RequestManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestManager
+{
+    public class RequestManager
+    {
+        private IRequestManager storage_;
+
+        RequestManager(IRequestManager storage)
+        {
+            storage_ = storage;
+        }
+
+        public string AddRequest(RequestModel request)
+        {
+            if (string.IsNullOrEmpty(request.Сustomer))
+            {
+                return "Введите заказчика";
+            }
+            if (string.IsNullOrEmpty(request.Condition))
+            {
+                return "Введите состояние заявки";
+            }
+            if (string.IsNullOrEmpty(request.Description))
+            {
+                return "Введите описание заявки";
+            }            
+
+            storage_.AddRequest(request);
+            return "Новая заявка успешно добавлена";
+        }
+
+        public string UpdateRequests(RequestModel request)
+        {
+            if (string.IsNullOrEmpty(request.Сustomer))
+            {
+                return "Введите заказчика";
+            }
+            if (string.IsNullOrEmpty(request.Condition))
+            {
+                return "Введите состояние заявки";
+            }
+            if (string.IsNullOrEmpty(request.Description))
+            {
+                return "Введите описание заявки";
+            }
+
+            storage_.AddRequest(request);
+            return "Новая заявка успешно добавлена";
+        }
+    }
+}
